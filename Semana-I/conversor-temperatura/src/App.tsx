7879acc7652ba2f2703a21d9b1e559b2e8251b90
esdras-josue <esdraspea@gmail.com>
2025-10-18 20:10:13 -0600

    "Tarea uno finalizada"

---
+++ Semana-I/conversor-temperatura/src/App.tsx
@@ -0,0 +1,50 @@
+import type { Conversion } from "./modelo/Conversion.ts";
+import "./App.css"
+
+function convertirTemperatura(valor: number, unidad: string): Conversion[] {
+
+  if(unidad === "Celcius") {
+    return [
+        { unidad: "Celcius", valor: valor},
+        { unidad: "Fahrenheit", valor: (valor * 9) / 5 + 32 },
+        { unidad: "Kelvin", valor: valor + 273.15 },
+    ];
+  }
+  else if(unidad === "Fahrenheit") {
+    return [
+        { unidad: "Fahrenheit", valor: valor },
+        { unidad: "Celcius", valor: ((valor - 32) * 5) / 9 },
+        { unidad: "Kelvin", valor: ((valor - 32) * 5) / 9 + 273.15 }
+    ]
+  }
+  else if(unidad === "Kelvin") {
+    return [
+        { unidad: "Kelvin", valor: valor },
+        { unidad: "Celcius", valor: valor - 273.15 },
+        { unidad: "Fahrenheit", valor: (valor - 273.15) * 9/5 + 32 }
+    ]
+  }
+  else{
+    return [];
+  }
+
+}
+
+function App() {
+  const temperaturaInicial = 25;
+  const conversiones = convertirTemperatura(temperaturaInicial, "Celcius");
+  return (
+    <div className="container">  
+      <h2>Conversión de Temperatura</h2>
+      <ul>
+        {conversiones.map((conversion) => (
+          <li key={conversion.unidad}>
+            {conversion.unidad}: {conversion.valor.toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div> 
+  )
+}
+
+export default App;
