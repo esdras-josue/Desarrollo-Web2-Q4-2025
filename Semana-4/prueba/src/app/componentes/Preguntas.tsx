dcc56d95982c519221e33438c975783445e2a3df
esdras-josue <esdraspea@gmail.com>
2025-11-12 18:00:51 -0600

    nueva semana

--- Semana-4/prueba/src/app/componentes/Preguntas.tsx
+++ Semana-4/prueba/src/app/componentes/Preguntas.tsx
@@ -1,117 +1,121 @@
-import React, { useEffect, useState } from 'react'
-import { useTriviaContext } from '../provider/TriviaProvider';
-import { Pregunta } from '../modelos/Pregunta';
+"use client";
+import React, { useEffect, useState } from "react";
+import { useTriviaContext } from "../provider/TriviaProvider";
+import { Pregunta } from "../modelos/Pregunta";
 
 export default function Preguntas() {
-    const { puntaje, incrementarPuntos, reiniciarJuego} = useTriviaContext();
-    const [preguntas, setPreguntas] = useState<Pregunta[]>([]);
-    const [indice, setIndice] = useState<number>(0);
-    const [seleccion, setSeleccion] = useState<boolean | null>();
-    const [mensaje, setMensaje] = useState("");
+  const { puntaje, incrementarPuntos, reiniciarJuego } = useTriviaContext();
+  const [preguntas, setPreguntas] = useState<Pregunta[]>([]);
+  const [indice, setIndice] = useState<number>(0);
+  const [seleccion, setSeleccion] = useState<boolean | null>();
+  const [mensaje, setMensaje] = useState("");
 
-    useEffect(() => {
-        const lista = [
-            {
-                idPregunta: 1,
-                descripcion: "Messi es el jugador con mas balones de oro.",
-                opcionRespuesta1: true,
-                opcionRespuesta2: false,
-                respuestaCorrecta: true,
-                puntos: 1,
-            },
-            {
-                idPregunta: 2,
-                descripcion : "Cristiano Ronaldo es el jugador con mas goles en la historia del futbol.",
-                opcionRespuesta1: true,
-                opcionRespuesta2: false,
-                respuestaCorrecta: true,
-                puntos: 1
-            },
-            {
-                idPregunta: 3,
-                descripcion: "El Real Madrid es el club con mas UEFA CHAMPIONS LEAGUE.",
-                opcionRespuesta1: false,
-                opcionRespuesta2: true,
-                respuestaCorrecta: true,
-                puntos: 1
+  useEffect(() => {
+    const lista = [
+      {
+        idPregunta: 1,
+        descripcion: "Messi es el jugador con mas balones de oro.",
+        opcionRespuesta1: true,
+        opcionRespuesta2: false,
+        respuestaCorrecta: true,
+        puntos: 1,
+      },
+      {
+        idPregunta: 2,
+        descripcion:
+          "Cristiano Ronaldo es el jugador con mas goles en la historia del futbol.",
+        opcionRespuesta1: true,
+        opcionRespuesta2: false,
+        respuestaCorrecta: true,
+        puntos: 1,
+      },
+      {
+        idPregunta: 3,
+        descripcion: "El Real Madrid es el club con mas UEFA CHAMPIONS LEAGUE.",
+        opcionRespuesta1: false,
+        opcionRespuesta2: true,
+        respuestaCorrecta: true,
+        puntos: 1,
+      },
+      {
+        idPregunta: 4,
+        descripcion: "Olimpia es el club mas galardonado en Honduras.",
+        opcionRespuesta1: true,
+        opcionRespuesta2: false,
+        respuestaCorrecta: true,
+        puntos: 1,
+      },
+      {
+        idPregunta: 5,
+        descripcion: "Honduras de ha clasificado a 5 mundiales de la FIFA.",
+        opcionRespuesta1: true,
+        opcionRespuesta2: false,
+        respuestaCorrecta: false,
+        puntos: 1,
+      },
+    ];
+    setPreguntas(lista);
+  }, []);
 
-            },
-            {
-                idPregunta: 4,
-                descripcion: "Olimpia es el club mas galardonado en Honduras.",
-                opcionRespuesta1: true,
-                opcionRespuesta2: false,
-                respuestaCorrecta: true,
-                puntos: 1
-            },
-            {
-                idPregunta: 5,
-                descripcion: "Honduras de ha clasificado a 5 mundiales de la FIFA.",
-                opcionRespuesta1: true,
-                opcionRespuesta2: false,
-                respuestaCorrecta: false,
-                puntos: 1
-
-            },
-        ];
-        setPreguntas(lista);
-    },[]);
-
-    function responder(opcion: boolean){
-        const actual = preguntas[indice];
-        setSeleccion(opcion);
-
-        if(opcion === actual.respuestaCorrecta){
-            incrementarPuntos();
-            setMensaje("Correcto");
-        } else{
-            setMensaje("Incorrecto");
-        }
-    }
-
-    function siguiente(){
-        setSeleccion(null);
-        setMensaje("");
-        if(indice < preguntas.length - 1){
-            setIndice(indice + 1) 
-        } else{
-            setMensaje("Hast terminado la trivia");
-        }
-    }
-
-    if( preguntas.length === 0) {
-        return <p>Cargando....</p>
-    }
-
+  function responder(opcion: boolean) {
     const actual = preguntas[indice];
+    setSeleccion(opcion);
+
+    if (opcion === actual.respuestaCorrecta) {
+      incrementarPuntos();
+      setMensaje("Correcto");
+    } else {
+      setMensaje("Incorrecto");
+    }
+  }
+
+  function siguiente() {
+    setSeleccion(null);
+    setMensaje("");
+    if (indice < preguntas.length - 1) {
+      setIndice(indice + 1);
+    } else {
+      setMensaje("Hast terminado la trivia");
+    }
+  }
+
+  if (preguntas.length === 0) {
+    return <p>Cargando....</p>;
+  }
+
+  const actual = preguntas[indice];
 
   return (
     <div>
-      <p> Pregunta {indice + 1} de {preguntas.length}</p>
+      <p>
+        Pregunta {indice + 1} de {preguntas.length}
+      </p>
       <p>{actual.descripcion}</p>
+      <div>
+        <button onClick={() => responder(true)} disabled={seleccion !== null}>
+          verdadero
+        </button>
+      </div>
 
-      <button onClick={() => responder(true)} disabled={seleccion !== null}>
-        verdadero
-      </button>
-
-      <button onClick={() => responder(true)} disabled={seleccion !== null}>
-        false
-      </button>
+      <div>
+        <button onClick={() => responder(true)} disabled={seleccion !== null}>
+          false
+        </button>
+      </div>
 
       {mensaje && (
         <div>
-            <p>{mensaje}</p>
-            {indice < preguntas.length -1 && (
-                <button onClick={siguiente}>Siguiente</button>
-            )}
-            {indice === preguntas.length -1 && (
-                <button onClick={reiniciarJuego}>Reiniciar Trivia</button>
-            )}
+          <p>{mensaje}</p>
+          {indice < preguntas.length - 1 && (
+            <button onClick={siguiente}>Siguiente</button>
+          )}
+          {indice === preguntas.length - 1 && (
+            <button onClick={reiniciarJuego}>Reiniciar Trivia</button>
+          )}
         </div>
       )}
 
       <p>Puntaje actual: {puntaje}</p>
     </div>
-  )
+  );
 }
-
--- Semana-4/prueba/src/app/provider/TriviaProvider.tsx
+++ Semana-4/prueba/src/app/provider/TriviaProvider.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState, useContext } from "react";
 import { PlantillaReact } from '../modelos/PlantillaReact'
 import { TriviaContext } from '../contexto/TriviaContext';
